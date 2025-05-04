030855c992f2e6bb9b5b7769d4fa1c5999cae8f4
akrivobokov <a.s.krivobokov@yandex.ru>
2025-05-04 10:31:01 +0300

    fix: добавлен TEMPLATES

--- business_management/settings.py
+++ business_management/settings.py
@@ -26,6 +26,21 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'business_management.urls'
 WSGI_APPLICATION = 'business_management.wsgi.application'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 DATABASES = {
     'default': {
